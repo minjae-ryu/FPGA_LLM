d09de97e0f87da22ee81c18efc2d4961ae27d107
minjae-ryu <ryu001108@gmail.com>
2025-12-03 12:11:22 +0900

    update some pipeline stage

--- src/PE_q5.v
+++ src/PE_q5.v
@@ -11,23 +11,21 @@ module PE_q5(
 wire signed [5-1:0] w_vec[32-1:0];
 wire signed [8-1:0] a_vec[32-1:0];
 
-reg signed  [5+8-1:0] mul[32-1:0];
+wire signed  [5+8-1:0] mul[32-1:0];
 genvar i;
 
 generate
     for(i = 0; i < 32; i = i + 1)begin
         assign w_vec[i] = w[(i+1)*5-1:i*5];
         assign a_vec[i] = a[(i+1)*8-1:i*8];
-        always @(posedge clk ) begin
-            mul[i] <= w_vec[i]*a_vec[i];
-        end
+        assign mul[i] <= w_vec[i]*a_vec[i];
     end
 endgenerate
 
 
-wire signed [14-1:0] ad0[16-1:0];
+reg signed [14-1:0] ad0[16-1:0];
 wire signed [15-1:0] ad1[8-1:0];
-reg  signed [16-1:0] ad2[4-1:0];
+wire  signed [16-1:0] ad2[4-1:0];
 wire signed [17-1:0] ad3[2-1:0];
 reg  signed [18-1:0] ad4;
 
@@ -35,7 +33,9 @@ genvar st0;
 
 generate
     for(st0 = 0; st0 < 16; st0 = st0 + 1)begin
-        assign ad0[st0] = mul[st0*2+1] + mul[st0*2];
+        always @(posedge clk ) begin
+        ad0[st0] <= mul[st0*2+1] + mul[st0*2];
+        end
     end
 endgenerate
 
@@ -49,9 +49,7 @@ endgenerate
 genvar st2;
 generate
     for(st2 = 0; st2 < 4; st2 = st2 + 1) begin
-        always @(posedge clk ) begin
-            ad2[st2] <= ad1[st2*2+1] + ad1[st2*2];
-        end
+        assign ad2[st2] <= ad1[st2*2+1] + ad1[st2*2];
     end
 endgenerate
 
